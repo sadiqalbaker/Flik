4881f3388e0de6c6219d6fea0453c3f74e1645ac
Sadiq Albaker <sadiqalbaker@gmail.com>
2022-06-06 13:36:24 +0300

    splash screen

---
+++ app/src/main/java/com/example/flik/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.flik
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setTheme(R.style.Theme_Flik)
+        setContentView(R.layout.activity_main)
+
+    }
+    }
